480acf8e1c511fa9083924de97025f3810aee713
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-12-20 23:47:06 +0200

    Update rotl.c

--- rotl.c
+++ rotl.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
 * f_rotl- function that rotates the stack to the top
 * @head: head of the stack
@@ -9,20 +8,19 @@
 */
 void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 {
-	stack_t *tmp = *head, *aux;
-
-	if (*head == NULL || (*head)->next == NULL)
-	{
-		return;
-	}
-	aux = (*head)->next;
-	aux->prev = NULL;
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-	tmp->next = *head;
-	(*head)->next = NULL;
-	(*head)->prev = tmp;
-	(*head) = aux;
+stack_t *tmp = *head, *aux;
+if (*head == NULL || (*head)->next == NULL)
+{
+return;
+}
+aux = (*head)->next;
+aux->prev = NULL;
+while (tmp->next != NULL)
+{
+tmp = tmp->next;
+}
+tmp->next = *head;
+(*head)->next = NULL;
+(*head)->prev = tmp;
+(*head) = aux;
 }
