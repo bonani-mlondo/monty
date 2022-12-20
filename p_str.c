b12f591c818213ae4fa3f1eb07b7cbde04280c5d
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-12-20 23:44:07 +0200

    Update p_str.c

--- p_str.c
+++ p_str.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
 * f_pstr - function that prints the string starting at the top of the stack,
 * followed by a new
@@ -10,18 +9,17 @@
 */
 void f_pstr(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	while (h)
-	{
-		if (h->n > 127 || h->n <= 0)
-		{
-			break;
-		}
-		printf("%c", h->n);
-		h = h->next;
-	}
-	printf("\n");
+stack_t *h;
+(void)counter;
+h = *head;
+while (h)
+{
+if (h->n > 127 || h->n <= 0)
+{
+break;
+}
+printf("%c", h->n);
+h = h->next;
+}
+printf("\n");
 }
