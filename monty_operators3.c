95f12a2620f17f655432ac3acefea05c3c7c5e79
Bonani Mlondo <103859173+bonani-mlondo@users.noreply.github.com>
2022-12-20 23:40:47 +0200

    Update monty_operators3.c

--- monty_operators3.c
+++ monty_operators3.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
 * f_pop - function that prints the top of the stack
 * @head: double head pointer to the stack
@@ -9,21 +8,19 @@
 */
 void f_pop(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	*head = h->next;
-	free(h);
+stack_t *h;
+if (*head == NULL)
+{
+fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
 }
-
+h = *head;
+*head = h->next;
+free(h);
+}
 /**
 * f_pint - function that prints the top of the stack
 * @head: double head pointer to the stack
@@ -33,17 +30,16 @@ void f_pop(stack_t **head, unsigned int counter)
 */
 void f_pint(stack_t **head, unsigned int counter)
 {
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*head)->n);
+if (*head == NULL)
+{
+fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+fclose(bus.file);
+free(bus.content);
+free_stack(*head);
+exit(EXIT_FAILURE);
 }
-
+printf("%d\n", (*head)->n);
+}
 /**
 * f_nop - function that does nothing
 * @head: double head pointer to the stack
@@ -53,6 +49,6 @@ void f_pint(stack_t **head, unsigned int counter)
 */
 void f_nop(stack_t **head, unsigned int counter)
 {
-	(void) counter;
-	(void) head;
+(void) counter;
+(void) head;
 }
